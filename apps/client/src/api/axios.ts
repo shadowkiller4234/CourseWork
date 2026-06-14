804990727192d4a0d18a963299d90e123a374bfc
Yaroslav <krasukyaroslav@gmail.com>
2026-06-14 22:20:28 +0300

    Update axios.ts

--- apps/client/src/api/axios.ts
+++ apps/client/src/api/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://coursework-1-pqt7.onrender.com",
   withCredentials: true
 });
